a558be25b58be41da55d6c0e67c4f5c36208b712
Benjamin Dietzkis <benjamin@iflix.com>
2018-01-24 10:11:31 +1100

    Initial Commit

---
+++ Analyzer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Analyzer
+//
+//  Created by Benjamin Dietzkis on 2018/01/24.
+//  Copyright © 2018 Benjamin Dietzkis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
